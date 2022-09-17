c4682cb7deee77637cb3329d950e4f8a6796d820
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2022-09-17 21:28:14 +0300

    update and delete data

--- Lab 2/display.php
+++ Lab 2/display.php
@@ -2,20 +2,28 @@
 <head>
     <title>Display Contacts</title>
     <link rel="stylesheet" href="styles.css" />
+    <!-- Add icons library -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <table class="table">
-        <thead class="table-heading">
-            <tr>
-                <th>#</th>
-                <th>Contact Name</th>
-                <th>Contact Profession</th>
-                <th>Telephone Number</th>
-                <th>Mobile Number</th>
-            </tr>
-        </thead>
-        <tbody>
-        
+    <div class="table">
+        <h2 class="form-title" align="center">All Contacts</h2>
+        <a class="update-btn" href="insert-form.html"><i class="fa fa-plus udpate-btn" aria-hidden="true"></i> Add</a>
+        <table class="">
+            <thead class="table-heading">
+                <tr>
+                    <th>#</th>
+                    <th>Contact Name</th>
+                    <th>Contact Profession</th>
+                    <th>Telephone Number</th>
+                    <th>Mobile Number</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            
         <?php
             // set your infomation.
             $host		=	'localhost';
@@ -30,22 +38,28 @@
             if ( $connect )
             {
                 mysql_select_db ( $database, $connect );
-                $sql = "SELECT * FROM `contacts`";
+                $sql = "SELECT * FROM `contacts` ORDER BY ID";
                 
                 if ( @mysql_query ( $sql) )
                 {
                     $query = mysql_query ( $sql );
 
                     $row = mysql_fetch_assoc ( $query );
+                   
                     do {
+                        $id = $row['ID'];
                         echo   
                         "<tr>
-                        <td>{$row['ID']}</td>
-                        <td>{$row['contact_name']}</td>
-                        <td>{$row['contact_profession']}</td>
-                        <td>{$row['telephone_number']}</td>
-                        <td>{$row['mobile_number']}</td>
-                    </tr>\n";
+                        <td name='id' align='center'>{$row['ID']}</td>
+                        <td name='contact_name'>{$row['contact_name']}</td>
+                        <td name='contact_profession'>{$row['contact_profession']}</td>
+                        <td name='telephone_number'>{$row['telephone_number']}</td>
+                        <td name='mobile_number'>{$row['mobile_number']}</td>
+                       <td align='center'><a class='update-btn' href='update-form.php?action=get&id=$id'>
+                            <i class='fa fa-pencil' aria-hidden='true'></i></a></td>
+                        <td align='center'><a class='delete-btn' href='delete.php?action=delete&id=$id'>
+                            <i class='fa fa-trash' aria-hidden='true'></i></a></td>
+                        </tr>\n";
                     } while ( $row = mysql_fetch_assoc ( $query ) );
 
                 }
@@ -59,6 +73,6 @@
         ?>
     </tbody>
 </table>
-
+        </div>
 </body>
 </html>
