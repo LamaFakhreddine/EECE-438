3718d6033de40d58aabf23b7f896723b13a8ed7c
Lama Fakhreddine <77051181+LamaFakhreddine@users.noreply.github.com>
2022-10-30 21:28:19 +0200

    first commit

---
+++ Lab 6/lab6/main/tests.py
@@ -0,0 +1,34 @@
+#from django.test import TestCase
+
+# Create your tests here.
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Create your tests here.
+class PlayerFormTest(LiveServerTestCase):
+
+  def testform(self):
+    driver = webdriver.Chrome(executable_path="C:\\bin\chromedriver")
+    #Choose your url to visit
+    driver.get('http://127.0.0.1:8000/')
+    #find the elements you need to submit form
+    player_name = driver.find_element('name', 'name')
+    player_height = driver.find_element('name', 'height')
+    player_team = driver.find_element('name', 'team')
+    player_ppg = driver.find_element('name', 'ppg')
+
+    submit = driver.find_element('name', 'submit_button')
+
+    #populate the form with data
+    player_name.send_keys('Lebron James')
+    player_team.send_keys('Los Angeles Lakers')
+    player_height.send_keys('6 feet 9 inches')
+    player_ppg.send_keys('25.7')
+
+    #submit form
+    submit.send_keys(Keys.RETURN)
+
+    #check result; page source looks at entire html document
+    assert 'Lebron James' in driver.page_source
+
